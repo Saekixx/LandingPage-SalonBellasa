e390744c24bb09690544cda06933b0f58a2777e5
jd-software <leandrohernandezjackdaniel@gmail.com>
2026-06-01 22:21:27 -0500

    refactor: mejorar la estructura del código y actualizar clases de estilo en el layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,10 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({ variable: "--font-geist-sans", subsets: ["latin"] });
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
@@ -23,7 +26,9 @@ export default function RootLayout({
       lang="es"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col font-sans antialiased">
+      <body
+        className={`${geistSans.className} min-h-full flex flex-col antialiased`}
+      >
         {children}
       </body>
     </html>
